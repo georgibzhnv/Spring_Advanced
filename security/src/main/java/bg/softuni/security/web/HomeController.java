f9758b61d4f77d15e22953940901c6233f4887a8
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-04-15 00:59:11 +0300

    Thymeleaf spring security extras

--- security/src/main/java/bg/softuni/security/web/HomeController.java
+++ security/src/main/java/bg/softuni/security/web/HomeController.java
@@ -1,21 +1,30 @@
 package bg.softuni.security.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         return "home";
     }
+
     @GetMapping("/admin")
-    public String admin(){
-        return "admin";
+    public ModelAndView admin(@AuthenticationPrincipal UserDetails principal) {
+        ModelAndView mav= new ModelAndView("admin");
+        mav.addObject("user", principal);
+        return mav;
     }
+
     @GetMapping("/user")
-    public String user(){
-        return "user";
+    public ModelAndView user(@AuthenticationPrincipal UserDetails principal) {
+        ModelAndView mav= new ModelAndView("user");
+        mav.addObject("user", principal);
+        return mav;
     }
 }
