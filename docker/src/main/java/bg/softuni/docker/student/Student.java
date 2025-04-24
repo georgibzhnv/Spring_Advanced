5c0c31b1ec3233e3300c66868c85e1db5ae2a4e8
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-04-24 21:12:54 +0300

    Initial commit.

---
+++ docker/src/main/java/bg/softuni/docker/student/Student.java
@@ -0,0 +1,54 @@
+package bg.softuni.docker.student;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Student setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Student setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Student setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Student setAge(int age) {
+        this.age = age;
+        return this;
+    }
+}
---
+++ docker/src/main/java/bg/softuni/docker/student/StudentController.java
@@ -0,0 +1,24 @@
+package bg.softuni.docker.student;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class StudentController {
+
+    private StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>>getAllStudents() {
+        return ResponseEntity.ok(studentRepository.findAll());
+    }
+
+}
